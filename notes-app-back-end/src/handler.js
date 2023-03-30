32acb6e238309f19a0a5b00ddac239c210f8b3bb
Ihsan An-Nashir <64590661+ihsanannashir@users.noreply.github.com>
2023-03-30 13:59:16 +0700

    delete notes

--- notes-app-back-end/src/handler.js
+++ notes-app-back-end/src/handler.js
@@ -59,6 +59,7 @@ const getNoteByIdHandler = (request, h) => {
       status: 'fail',
       message: 'Catatan tidak ditemukan',
     });
+
     response.code(404);
     return response;
 };
@@ -91,8 +92,33 @@ const editNoteByIdHandler = (request, h) => {
         status: 'fail',
         message: 'Gagal memperbarui catatan. Id tidak ditemukan',
     });
+
     response.code(404);
     return response;
 };
 
-module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler };
+const deleteNoteByIdHandler = (request, h) => {
+    const { id } = request.params;
+   
+    const index = notes.findIndex((note) => note.id === id);
+   
+    if (index !== -1) {
+      notes.splice(index, 1);
+      const response = h.response({
+        status: 'success',
+        message: 'Catatan berhasil dihapus',
+      });
+      response.code(200);
+      return response;
+    }
+   
+   const response = h.response({
+      status: 'fail',
+      message: 'Catatan gagal dihapus. Id tidak ditemukan',
+    });
+
+    response.code(404);
+    return response;
+};
+
+module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler };
